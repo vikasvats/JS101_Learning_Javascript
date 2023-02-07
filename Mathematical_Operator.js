11ffaca3c55dc311166a8cc68e30b03e92e202a1
vikasvats <vikas202319@gmail.com>
2023-02-08 00:36:46 +0530

    Update Mathematical_Operator.js
    hey friends i changed the code

--- Mathematical_Operator.js
+++ Mathematical_Operator.js
@@ -1,12 +1,8 @@
-//print the prime number between 0 to given limit
-let num=1010
-for(let i=0;i<num;i++){
-let count=0
-  for(let j=2;j<=num;j++){
-    if(i%j===0 && i!==j){
-      count++
-    }
-}if(count==0){
-  console.log(i,"bhai ye prime hai")
-  }
+//print the age group
+let age= 15
+if(age<=13 || age>=19){
+  console.log("Teenage")
+}
+if(age<=20 || age>=29){
+  console.log("Twenties")
 }
